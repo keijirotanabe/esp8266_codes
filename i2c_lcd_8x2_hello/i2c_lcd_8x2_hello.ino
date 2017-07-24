6b717a8adbe84366f37a699887cc3f5bd00f9cf2
Keijiro Tanabe <ayt7gysnyx@gmail.com>
2017-07-24 16:21:26 +0900

    update 「i2c_lcd_8x2_hello」

--- i2c_lcd_8x2_hello/i2c_lcd_8x2_hello.ino
+++ i2c_lcd_8x2_hello/i2c_lcd_8x2_hello.ino
@@ -6,9 +6,10 @@ ST7032 lcd(0x3E);     // スレーブアドレスを 0x3E に設定
 void setup() {
   lcd.begin(8, 2);       // lcd の使用宣言 (8x2)
   lcd.setContrast(30);   // コントラストの設定
-  lcd.print("Hello!");   // 「」と表示
-  lcd.setCursor(1, 0);   // カーソルを2行目の1列目に移動
-  lcd.print("ESP8266");  // 「」と表示
+  lcd.setCursor(0, 0);   // カーソルを1列目の1行目に移動
+  lcd.print("Hello!");   // 「Hello!」と表示
+  lcd.setCursor(0, 1);   // カーソルを1列目の2行目に移動
+  lcd.print("ESP8266");  // 「ESP8266」と表示
 }
 
 void loop() {
