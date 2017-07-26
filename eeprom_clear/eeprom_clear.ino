165a38dc5e62f786e2c2fba4dd9981942ad95d74
keijirotanabe <ayt7gysnyx@gmail.com>
2017-07-26 12:46:49 +0900

    Update eeprom_clear.ino

--- eeprom_clear/eeprom_clear.ino
+++ eeprom_clear/eeprom_clear.ino
@@ -4,7 +4,7 @@ void setup() {
   Serial.begin(115200);   // シリアル通信を115200bpsで使用
   EEPROM.begin(512);      // EEPROM の 512バイトを使用
 
-  // msg[] の文字配列を EEPROM へ変更を保存
+  // EEPROMの0~511番地に0を書き込む
   for (int i = 0; i < 512; i++) {
     EEPROM.write(i, 0);
   }
